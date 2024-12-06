cbb38452d2b442927b95c0b48bcdc272dfa9ee92
Johnny Tran <48148643+johnnytran0@users.noreply.github.com>
2024-12-06 15:50:01 -0800

    optimize 2024 day 6 part 2 solution; simplify orientation logic; add multiprocessing

--- aoc/2024/06/part2.py
+++ aoc/2024/06/part2.py
@@ -1,17 +1,17 @@
 from aoc.puzzle import Part2
 from copy import deepcopy
-import pprint
+import multiprocessing
+import time
 
-# clockwise rotation: up, right, down, left, ...
-guards = ('^', '>', 'v', '<')
-direction = ('north', 'east', 'south', 'west')
+# clockwise order
+directions = ('north', 'east', 'south', 'west')
 obstacle = '#'
 visited = 'X'
 
 def rotate_ccw_90(matrix, coords, orientation):
     """
-    rotates the matrix 90 degrees clockwise and transfer coordinates to rotated matrix.
-    Does not reorient guard direction.
+    rotates the matrix 90 degrees counterclockwise and transfer coordinates to rotated matrix.
+    does not rotate the orientation of the guard so guard's perspective turns to the right
     :param matrix: 2d array
     :param coords: a pair of coordinates to transfer
     :return: rotated 2d array and transferred coordinates
@@ -19,115 +19,75 @@ def rotate_ccw_90(matrix, coords, orientation):
     (x, y)  = coords
     rotated_coords = (len(matrix) - 1 - y, x)
     rotated_matrix = [list(col) for col in zip(*matrix)][::-1]
-    rotated_orientation = direction[(direction.index(orientation) + 1) % 4]
+    rotated_orientation = directions[(directions.index(orientation) + 1) % 4]
     return rotated_matrix, rotated_coords, rotated_orientation
-
-def rotate_cw_90(matrix, coords, orientation):
-    """
-    rotates the matrix 90 degrees clockwise and transfer coordinates to rotated matrix.
-    Does not reorient guard direction.
-    :param matrix: 2d array
-    :param coords: a pair of coordinates to transfer
-    :return: rotated 2d array and transferred coordinates
-    """
-    (x, y)  = coords
-    rotated_coords = (y, len(matrix) - 1 - x)
-    rotated_matrix = [list(col)[::-1] for col in zip(*matrix)]
-    rotated_orientation = direction[(direction.index(orientation) - 1) % 4]
-    return rotated_matrix, rotated_coords, rotated_orientation
-
-def reorient_map(map, coords, orientation):
-    ## reorient map
-    rotated_map, rotated_coords, rotated_orientation = rotate_cw_90(map, coords, orientation)
-
-    # reorient guard direction CCW, so that guard is always travelling to the right through the columns
-    next_guard_dir_index = (guards.index(rotated_map[rotated_coords[0]][rotated_coords[1]][0]) + 1) % 4
-    rotated_map[rotated_coords[0]][rotated_coords[1]] = (guards[next_guard_dir_index], rotated_map[rotated_coords[0]][rotated_coords[1]][1])
-
-    return rotated_map, rotated_coords, rotated_orientation
 
 def patrol(map, coords, orientation):
     """
     Lab guards follow protocol:
     - If there is something directly in front of you, turn right 90 degrees.
     - Otherwise, take a step forward.
-
     :param map: requires that guard must always be moving to the right, >
     :return: visited map and final patrol coordinates, None if out of bounds?
     """
     (x, y) = coords
 
-    last_col_y = len(map[0])-1
-
-    if y + 1 <= last_col_y and map[x][y+1][0] == obstacle:
-        if map[x][y+1][0] == obstacle:
+    if 0 <= x-1 and map[x-1][y][0] == obstacle:
+        if map[x-1][y][0] == obstacle:
             # check if obstacle has been visited from this direction
-            guard_orientation = direction[(direction.index(orientation) + 1) % 4]
-            if guard_orientation in map[x][y+1][1]:
+            guard_orientation = orientation
+            if guard_orientation in map[x-1][y][1]:
                 # found loop because obstacle has already been visited from this orientation
                 return None, None, None, True
             else:
                 # indicates obstacle has been visited from this orientation
-                map[x][y+1][1].add(guard_orientation)
+                map[x - 1][y][1].add(guard_orientation)
         # turn guard right 90 degrees
-        right_turn_map, right_turn_coords, right_turn_orientation= rotate_ccw_90(map, coords, orientation)
-        return right_turn_map, right_turn_coords, right_turn_orientation, False
+        return rotate_ccw_90(map, coords, orientation), False
     else:
         # mark current node as visited
         guard = map[x][y][0]
         map[x][y] = (visited, None)
-        if y + 1 <= last_col_y:
-            # take next step to the right
-            map[x][y+1] = (guard, None)
-            return map, (x, y+1), orientation, False
+        if 0 <= x-1:
+            # take a step forward
+            map[x-1][y] = (guard, None)
+            return map, (x-1, y), orientation, False
         else:
             # guard has left the boundaries of map
             return map, None, orientation, False
 
-def find_loop(map, coords, orientation):
-    rotated_map, rotated_coords, rotated_orientation = reorient_map(map, coords, orientation)
-    while rotated_coords:
-        rotated_map, rotated_coords, rotated_orientation, has_loop = patrol(rotated_map, rotated_coords, rotated_orientation)
-        if has_loop:
-            return True
-    return False
-
 class PuzzlePart2(Part2):
+    def find_loop(self, obstruction_coords):
+        # create obstruction map to track visits
+        obstructed_map = deepcopy(self.map)
+        obstructed_map[obstruction_coords[0]][obstruction_coords[1]] = obstacle
+
+        # replaces map cell with a tuple for tracking visits to an obstacle
+        visited_map = [[(val, set() if val == obstacle else None) for val in rows] for rows in obstructed_map]
+        coords = self.guard_coords
+        orientation = directions[0]
+        while coords:
+            visited_map, coords, orientation, has_loop = patrol(visited_map, coords, orientation)
+            if has_loop:
+                return True
+        return False
+
     def solve(self, input_str: str):
         """
         assumes initial guard direction is up, ^
         :param input_str:
         :return:
         """
-        map = [[z for z in row.strip()] for row in input_str.strip().splitlines()]
-        # pprint.pprint(map)
-        rows, cols = len(map), len(map[0])
+        self.map = [[val for val in row.strip()] for row in input_str.strip().splitlines()]
+        rows, cols = len(self.map), len(self.map[0])
 
-        # find initial position of guard
-        guard_coords = None
-        orientation = direction[0]
-        for x in range(rows):
-            for y in range(cols):
-                if map[x][y] in guards:
-                    guard_coords = (x, y)
-                    break
+        self.guard_coords = next((x, y) for x in range(rows) for y in range(cols) if self.map[x][y] == '^')
 
-        obstructed_loops = 0
-        print(f'rows: {rows}, cols: {cols}')
-        for row in range(rows):
-            for col in range(cols):
-                cell = map[row][col]
-                if cell == '.':
-                    # print(f'{row, col}: {cell} overwritten with obstacle')
+        obstruction_coords = [(x, y) for x in range(rows) for y in range(cols) if self.map[x][y] == '.']
 
-                    obstructed_map = deepcopy(map)
-                    obstructed_map[row][col] = obstacle
-
-                    visited_map = [[(val, set() if val == obstacle else None) for val in rows] for rows in obstructed_map]
-                    if find_loop(visited_map, guard_coords, orientation):
-                        print(f'obstruction at {row, col} causes a loop')
-                        obstructed_loops += 1
-        return obstructed_loops
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+            # call the same function with different data in parallel
+            return sum(pool.map(self.find_loop, obstruction_coords))
 
 if __name__ == '__main__':
     puzzle = PuzzlePart2(2024, 6)
@@ -146,6 +106,9 @@ if __name__ == '__main__':
         '''
     assert 6 == puzzle.solve(example_input)
 
+    start = time.time()
     answer = puzzle.solve(puzzle.input())
+    print(f'completed in {time.time() - start} seconds')
+
     print(f'answer: {answer}')
     assert 1434 == answer
